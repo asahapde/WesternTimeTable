ff2308ecdf44c48f974680fedeb8c5fff5827edc
asahapde <60831459+asahapde@users.noreply.github.com>
2020-12-02 00:50:43 -0500

    Added register method

--- backend/routes/open.js
+++ backend/routes/open.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 router.get('/', (req,res) => {
     res.send('From Open Route');
+})
+
+router.post('/register', (req,res) => {
+    let userData = req.body;
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.status(200).send(registeredUser);
+        }
+    })
+})
+
+router.post('/login', (req,res) => {
 })
 
 module.exports = router;

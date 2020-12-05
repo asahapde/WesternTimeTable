09d6d46da1209ac6ae9d3bf6e8c416f0998bb15d
asahapde <60831459+asahapde@users.noreply.github.com>
2020-12-05 03:25:05 -0500

    Edited auth guard

--- frontend/western-timetable/src/app/admin.guard.ts
+++ frontend/western-timetable/src/app/admin.guard.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router : Router) {
+    
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (!this.authService.isLoggedIn()) {
+        this.router.navigateByUrl('/login');
+        this.authService.deleteToken();
+        return false;
+      }
     return true;
   }
   
